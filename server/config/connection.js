b79865ac42dce26c00e0dde6ec41fdf0854cf34e
Zekun Chen <zekunchen97@gmail.com>
2022-08-22 01:23:44 +0930

    dpl2

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const path = require('path');
-require('dotenv').config({path: path.join(__dirname, '../.env' )});
+require('dotenv').config({path: path.join(__dirname, './.env' )});
 
 //mongodb+srv://test:test@cluster0.f92hqfr.mongodb.net/?retryWrites=true&w=majority
 //mongodb+srv://test:test@cluster0.f92hqfr.mongodb.net/final-project-test?retryWrites=true&w=majority
 //mongodb://127.0.0.1:27017/final-project-test
+// require('dotenv').config({path: path.join(__dirname, '../.env' )});
+
 
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/final-project-test', {
