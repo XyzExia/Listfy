d9cd4ec02a85fc5ae1d63156180143b4fe0ad23f
Zekun Chen <zekunchen97@gmail.com>
2022-08-22 04:14:27 +0930

    dpl get

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 console.log(process.env.NODE_ENV)
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
